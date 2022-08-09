10a5af84a71031b32adbd42d5aa3763b4d18e438
1807869 <yysun2000@thehyundai.com>
2022-08-09 21:23:57 +0900

    vercel

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,5 +3,13 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve:{
+   alias:[
+     {
+       find: 'react-query/devtools',
+       replacement: 'react-query/es/devtools/index',
+     }
+   ]
+  }
 })
